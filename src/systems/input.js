0f149daaa997634ff8fd9c08f95a486878b26078
ValeryDariana <valerydariana98@gmail.com>
2026-04-08 17:14:16 -0400

    feat: implementar control de teclado para inputs

--- src/systems/input.js
+++ src/systems/input.js
@@ -1,2 +1,16 @@
 // Manejo de entrada del usuario.
 // Detecta teclado y guarda el estado de las teclas presionadas.
+
+export const keys = {};
+
+export function initInput() {
+    //console.log("input iniciado");
+
+    window.addEventListener("keydown", (e) => {
+        keys[e.code] = true;
+    });
+
+    window.addEventListener("keyup", (e) => {
+        keys[e.code] = false;
+    });
+}
